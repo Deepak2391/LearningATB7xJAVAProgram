ca9ab939c8800e29d0db3bdee1397af7fe451cee
Deepak2391 <Deepak.gour23@gmail.com>
2024-07-08 23:17:54 +0530

    Add New Lab009

---
+++ src/ex_07072024/Lab009.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        System.out.println(1*1);
+        System.out.println(1*2);
+        System.out.println(1*3);
+        System.out.println(1*4);
+        System.out.println(1*5);
+    }
+}
