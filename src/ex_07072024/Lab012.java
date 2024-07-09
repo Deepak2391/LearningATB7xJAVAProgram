7dacc751824c0a02ddc90d0e120d3a8e5f6afc1c
Deepak2391 <Deepak.gour23@gmail.com>
2024-07-09 18:00:44 +0530

    Add New Lab012.java

---
+++ src/ex_07072024/Lab012.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 == 0)
+            System.out.println (num +" Is Even" );
+        else
+            System.out.println (num +" Is Odd" );
+    }
+}
